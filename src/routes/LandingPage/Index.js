cc6179cd647c2173c45c0b2036e42fc5485faf55
Zeshan <53648401+MohammadZeeshanQ@users.noreply.github.com>
2021-08-05 12:21:50 +0300

    Add: PopUp

--- src/routes/LandingPage/Index.js
+++ src/routes/LandingPage/Index.js
@@ -1,16 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Dialog, DialogContent, DialogActions, Typography, Button } from '@material-ui/core'
+
 import LandingPage from './LandingPage';
 import Feature from './Features.js';
 import SideBySide from './SideBySide.js';
-import { makeStyles } from '@material-ui/styles';
 
-// '@media(max-width: 600px)': {
-
-// }
-
-// '@media(min-width: 600px) and (max-width: 900px)': {
-
-// }
 
 const useStyles = makeStyles({
     root: {
@@ -20,17 +15,46 @@ const useStyles = makeStyles({
         '@media(max-width: 600px)': {
             width: '90%',
         }
-    }
+    },
+
+    header: {
+        textAlign: 'center',
+        fontFamily: 'source-Semi',
+        letterSpacing: '1px',
+        lineHeight: '150%',
+    },
 });
 
 
 export default function Index() {
     const classes = useStyles();
+    const [dialog, setDialog] = useState(true);
 
-    console.log('v-1.0');
+    const dialogHandler = () => {
+        setDialog(!dialog);
+    };
 
     return (
         <div className={classes.root}>
+
+            <Dialog
+                open={dialog}
+                onClose={dialogHandler}
+            >
+                <DialogContent>
+                    <Typography className={classes.header} variant='h5'>
+                        This is just a UI design the tabs button links are not Working As there are no Routes assigned to them.
+                        <br />
+                        <br />Thank You!!
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={dialogHandler} color='secondary' variant='contained' size='medium'>
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
             <LandingPage />
             <Feature />
             <SideBySide />
